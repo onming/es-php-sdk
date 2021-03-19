1f9cb0fb241843aeeab3ddabd264f8eb4348e5dc
zhangxueming <170893265@qq.com>
2021-03-19 18:27:24 +0800

    完善 索引管理、文档管理、查询等功能

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ elasticsearch sdk插件
 将会陆续更新
 
 ## 环境准备
-*   PHP 7.2+
+*   PHP 7.0+
     您可以通过`php -v`命令查看当前的 PHP 版本。
 *   curl 扩展
     您可以通过`php -m`命令查看 curl 扩展是否已经安装好。
-*   安装ELK
+*   安装ELK 6.0+
     安装文档https://www.jianshu.com/p/1b1578af7ba0
 
 ## 安装方式 
@@ -19,7 +19,83 @@ composer require onming/es-php-sdk ~1.0
 ### 使用方法
 ```
 use Onming\EsPhpSdk\Client;
-$client = new Client('127.0.0.1:9200'); // 设置elasticsearch主机
-$result = $client->getAnalyze('中文分词测试'); // url参数可以动态传递否则默认取当前访问链接
-var_dump($signPackage);
+
+$hosts = [
+    'host' => '127.0.0.1',
+    'port' => '9200',
+    'scheme' => 'http',
+    'user' => '',
+    'pass' => '',
+];
+$client = new Client($hosts);
+// 测试Analyze
+$result = $client->getAnalyze('测试中文分词');
+var_dump($result);
+// 删除索引
+$result = $client->deleteIndex('test_index');
+var_dump($result);
+// 创建索引
+// +----------------------------------------------------------------------
+// | 类型参考
+// | text:全文检索，分词
+// | keyword:精确查询不分词
+// | Intger:整形
+// | float:浮点
+// | nested:存储array或json
+// | date:日期类型 格式化 yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis
+// | geo_point:用来存储经纬度
+// | ip:可以存储IPV4 和IPV6
+// +----------------------------------------------------------------------
+$properties = [
+    'md5' => [
+        'type' => 'keyword',
+    ],
+    'tag' => [
+        'type' => 'text',
+        'analyzer' => 'ik_max_word',
+        'index' => true,
+        'store' => false,
+    ],
+    'scale' => [
+        'type' => 'float',
+    ],
+    'position' => [
+        'type' => 'geo_point',
+    ],
+    'caeate_at' => [
+        'type' => 'date',
+        'format' => 'yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis',
+    ],
+];
+$result = $client->createIndex('test_index', 'test_type', $properties);
+var_dump($result);
+// 添加文档
+$body = [
+    'md5' => '111111',
+    'tag' => '狗,宠物,动物,户外,狗狗,哺乳动物,毛茸茸,雪,可爱,自然',
+    'scale' => 1.1,
+    'position' => ['lon' => 116.111, 'lat' => 39.111],
+    'create_at' => date('Y-m-d H:i:s'),
+];
+$result = $client->insert('test_index', 'test_type', '1', $body);
+var_dump($result);
+$body = [
+    'md5' => '222222',
+    'tag' => '老虎,宠物,动物,户外,狗狗,哺乳动物,毛茸茸,雪,可爱,自然',
+    'scale' => 0.8,
+    'position' => ['lon' => 116.222, 'lat' => 39.222],
+    'create_at' => date('Y-m-d H:i:s'),
+];
+$result = $client->insert('test_index', 'test_type', '2', $body);
+var_dump($result);
+// 删除文档
+$result = $client->delete('test_index', 'test_type', '1');
+var_dump($result);
+// 查询所有
+$result = $client->search('test_index', 'test_type', 'all');
+// 按条件查询排序
+$match = ['tag' => '宠物狗'];
+$sort = ['_score' => 'desc'];
+$result = $client->search('test_index', 'test_type', $match, 0, 100, $sort);
+var_dump($result);
 ```
--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,7 @@
 
 namespace Onming\EsPhpSdk;
 
+use Exception;
 use Onming\EsPhpSdk\Helper\Http;
 
 /**
@@ -12,24 +13,160 @@ use Onming\EsPhpSdk\Helper\Http;
  */
 class Client
 {
-    private $elk_host;
+    protected $elk_host;
+    protected $elk_auth;
 
-    public function __construct($host)
+    public function __construct($config)
     {
-        $this->elk_host = "http://{$host}/";
+        $this->elk_host = $config['scheme'].'://'.$config['host'].':'.$config['port'].'/';
+        if($config['user'] && $config['pass']){
+            $this->elk_auth = $config['user'].':'.$config['pass'];
+        }
+    }
+
+    /**
+     * 创建索引
+     * 
+     * @param string index 索引名称
+     * @param string doc 文档名称
+     * @param array properties 数据结构
+     * @param int number_of_shards 分片数
+     * @param int number_of_replicas 备份数
+     * @return bool
+     */
+    public function createIndex($index, $doc, $properties, $number_of_shards = 1, $number_of_replicas = 0)
+    {
+        $params = [
+            'settings' => [
+                'number_of_shards' => $number_of_shards,
+                'number_of_replicas' => $number_of_replicas,
+            ],
+            'mappings' => [
+                $doc => ['properties' => $properties]
+            ]
+        ];
+        $response = $this->sendRequest($index, $params, 'PUT');
+        return !empty($response['acknowledged'])?true:false;
+    }
+
+    /**
+     * 获取索引配置及数据结构
+     * 
+     * @param string index 索引名称
+     * @return array
+     */
+    public function getIndex($index)
+    {
+        return $this->sendRequest($index, '', 'GET');
+    }
+
+    /**
+     * 删除索引
+     * 
+     * @param string index 索引名称
+     * @return bool
+     */
+    public function deleteIndex($index)
+    {
+        $response = $this->sendRequest($index, '', 'DELETE');
+        return !empty($response['acknowledged'])?true:false;
+    }
+
+    /**
+     * 添加文档
+     * 
+     * @param string index 索引名称
+     * @param string doc 文档名称
+     * @param string id 文档ID
+     * @param array body 内容
+     * @return string
+     */
+    public function insert($index, $doc, $id, $body)
+    {
+        $params = $body;
+        $response = $this->sendRequest($index.'/'.$doc.'/'.$id, $params, 'PUT');
+        return !empty($response['_id'])?$response['_id']:'';
+    }
+
+    /**
+     * 删除文档
+     * 
+     * @param string index 索引名称
+     * @param string doc 文档名称
+     * @param string id 文档ID
+     * @return bool
+     */
+    public function delete($index, $doc, $id)
+    {
+        $response = $this->sendRequest($index.'/'.$doc.'/'.$id, '', 'DELETE');
+        return $response['result']=='deleted'?true:false;
+    }
+
+    /**
+     * 查询文档
+     * 
+     * @param string index 索引名称
+     * @param string doc 文档名称
+     * @param array match 查询条件
+     * @param int from 起始位置
+     * @param int size 查询数量
+     * @param int sort 排序条件
+     * @return array
+     */
+    public function search($index, $doc, $match = '', $from = 0, $size = 100, $sort = '')
+    {
+        $params = [
+            'query' => [],
+            'from' => $from,
+            'size' => $size,
+        ];
+        if($match == 'all'){
+            $params['query']['match_all'] = new \stdClass();
+        }else{
+            $params['query']['match'] = $match;
+        }
+        if($sort){
+            $params['sort'] = $sort;
+        }
+        $response = $this->sendRequest($index.'/'.$doc.'/_search', $params, 'POST');
+        $result = ['total' => 0, 'list' => []];
+        if(!empty($response['hits'])){
+            return ['total' => $response['hits']['total'], 'list' => $response['hits']['hits']];
+        }
+        return $result;
     }
 
     /**
      * 中文分词
+     * 
+     * @param  string words 关键词
+     * @return array
      */
     public function getAnalyze($words)
     {
-        $data = [
+        $params = [
             "analyzer" => "ik_max_word",
             "text" => $words
         ];
-        $result = Http::post($this->elk_host.'_analyze', json_encode($data));
-        return json_decode($result, true);
+        return $this->sendRequest('_analyze', $params, 'POST');
+    }
+
+    protected function sendRequest($url, $params, $method)
+    {
+        $http_header[] = 'Content-Type: application/json';
+        if($this->elk_auth){
+            $http_header[] = 'Authorization: Basic '.base64_encode($this->elk_auth);
+        }
+        $options = [CURLOPT_HTTPHEADER => $http_header];
+        if($params){
+            $params = json_encode($params);
+        }
+        $response = Http::sendRequest($this->elk_host.$url, $params, $method, $options);
+        if($response['ret']){
+            return json_decode($response['msg'], true);
+        }else{
+            throw new Exception($response['msg']);
+        }
     }
 
 }
